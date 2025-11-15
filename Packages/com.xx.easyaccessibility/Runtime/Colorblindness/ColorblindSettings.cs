6f3135fc86d8d6bdb6761e7d7d720e8dde2d1545
mainMachine\a-la-code <jerdstevens@gmail.com>
2025-11-15 10:10:39 -0500

    Initial implementation of colorblindness support: * Added a ScriptableRendererFeature for handling colorblindness correction * Implemented two versions of colorblindness correction:   - LUT (lookup table, faster but bigger file size)   - Procedural (generated, slower but smaller file size); still WIP * Created a script for generating LUTs

---
+++ Packages/com.xx.easyaccessibility/Runtime/Colorblindness/ColorblindSettings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace EasyAccessibility
+{
+    public static class ColorblindSettings
+    {
+        public enum ColorblindMode
+        {
+            None = 0,
+            Protanopia = 1,
+            Deutranopia = 2,
+            Tritanopia = 3,
+        };
+    }
+}
