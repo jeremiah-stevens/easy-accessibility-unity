37d3ea79034735a758bd63a84bb3d177c83b688e
jeremiah-stevens <jerdstevens@gmail.com>
2026-05-07 13:25:58 -0400

    Unity upgrade and some minor tweaks to the rebindable keys scene

--- Assets/TestObjects/RebindableKeys/Scripts/RebindableConflictResolution.cs
+++ Assets/TestObjects/RebindableKeys/Scripts/RebindableConflictResolution.cs
@@ -1,20 +1,53 @@
 using EasyAccessibility;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class RebindableConflictResolution : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI conflictText;
     BindingConflict conflict;
 
+    GameObject prevActiveObject;
+
     public void ResolveConflict()
     {
         this.gameObject.SetActive(true);
+        prevActiveObject = EventSystem.current.currentSelectedGameObject;
+        EventSystem.current.SetSelectedGameObject(this.GetComponentInChildren<Button>()?.gameObject);
         this.conflict = RebindableInputManager.Instance.CurrentConflict;
         conflictText.text = conflict.GetConflictDescription();
     }
 
-    public void ResolveConflictBlock() { RebindableInputManager.Instance.ResolveConflictBlock(conflict); this.gameObject.SetActive(false); }
-    public void ResolveConflictClear() { RebindableInputManager.Instance.ResolveConflictClear(conflict); this.gameObject.SetActive(false); }
-    public void ResolveConflictSwap() { RebindableInputManager.Instance.ResolveConflictSwap(conflict); this.gameObject.SetActive(false); }
+    public void ResolveConflictBlock()
+    {
+        RebindableInputManager.Instance.ResolveConflictBlock(conflict);
+        if(prevActiveObject)
+        {
+            EventSystem.current.SetSelectedGameObject(prevActiveObject);
+            prevActiveObject = null;
+        }
+        this.gameObject.SetActive(false);
+    }
+    public void ResolveConflictClear()
+    {
+        RebindableInputManager.Instance.ResolveConflictClear(conflict);
+        if(prevActiveObject)
+        {
+            EventSystem.current.SetSelectedGameObject(prevActiveObject);
+            prevActiveObject = null;
+        }
+        this.gameObject.SetActive(false);
+    }
+    public void ResolveConflictSwap()
+    {
+        RebindableInputManager.Instance.ResolveConflictSwap(conflict);
+        if(prevActiveObject)
+        {
+            EventSystem.current.SetSelectedGameObject(prevActiveObject);
+            prevActiveObject = null;
+        }
+        this.gameObject.SetActive(false);
+    }
 }
